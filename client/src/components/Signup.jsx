d6c59eee37998430c39377f09b820b2077d23d7c
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-04-11 17:29:53 -0700

    Added functionality to signup page.

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -6,20 +6,40 @@ import FormControl from 'react-bootstrap/lib/FormControl';
 import Checkbox from 'react-bootstrap/lib/Checkbox';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 
 class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+      username: '',
+      email: '',
+      password: ''
+
+    };
 
   }
 
-  //Function triggers when component rendered.
-  //If not needed it could be deleted.
-  componentDidMount() {
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value });
+    console.log(this.state.username);
   }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+    console.log(this.state.email);
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+    console.log(this.state.password);
+  }
+
+  handleClick() {
+    console.log("I have been clicked!!!");
+    this.props.handleSubmit(this.state.username, this.state.email, this.state.password);
+  }
+
 
   //Look at https://react-bootstrap.github.io/components/forms/
   //Issues with CSS needs to be resolved.
@@ -32,7 +52,11 @@ class Signup extends React.Component {
             Username
           </Col>
           <Col sm={10}>
-            <FormControl type="text" placeholder="Username" />
+            <FormControl
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsernameChange.bind(this)}
+            placeholder="Username" />
           </Col>
         </FormGroup>
 
@@ -41,7 +65,11 @@ class Signup extends React.Component {
             Email
           </Col>
           <Col sm={10}>
-            <FormControl type="email" placeholder="Email" />
+            <FormControl
+            type="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange.bind(this)}
+            placeholder="Email" />
           </Col>
         </FormGroup>
 
@@ -50,13 +78,17 @@ class Signup extends React.Component {
             Password
           </Col>
           <Col sm={10}>
-            <FormControl type="password" placeholder="Password" />
+            <FormControl
+            type="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange.bind(this)}
+            placeholder="Password" />
           </Col>
         </FormGroup>
 
         <FormGroup>
           <Col smOffset={2} sm={10}>
-            <Button type="submit">Sign up</Button>
+            <Button type="submit" onClick={this.handleClick.bind(this)}>Sign up</Button>
           </Col>
         </FormGroup>
       </Form>
